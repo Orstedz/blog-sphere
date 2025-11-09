5e09da982fb542c752fa0e42b3cf79cfb02bd0cd
Minh Pham <giaminh2042004@gmail.com>
2025-11-09 12:02:55 +0700

    Setup Migration for database

---
+++ src/BlogSphere.Data/BlogSphereContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogSphere.Data
+{
+    public class TeduBlogContextFactory : IDesignTimeDbContextFactory<BlogSphereContext>
+    {
+        public BlogSphereContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<BlogSphereContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new BlogSphereContext(builder.Options);
+        }
+    }
+}
