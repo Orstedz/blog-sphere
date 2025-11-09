5dddd69bbf47799b7f75fc21d8e313231d0b67f7
Minh Pham <giaminh2042004@gmail.com>
2025-11-09 21:50:38 +0700

    Implement paging

--- src/BlogSphere.Core/Repositories/IPostRepository.cs
+++ src/BlogSphere.Core/Repositories/IPostRepository.cs
@@ -1,10 +1,12 @@
 ﻿using BlogSphere.Core.Domain.Content;
+using BlogSphere.Core.Models;
 using BlogSphere.Core.SeedWorks;
 
 namespace BlogSphere.Core.Repositories
 {
     public interface IPostRepository : IRepository<Post, Guid>
     {
-        Task<List<Post>> GetPopularPostAsync(int count);
+        Task<List<Post>> GetPopularPostsAsync(int count);
+        Task<PagedResult<Post>> GetPostsPagingAsync(string keyword, Guid? categoryId, int pageIndex = 1, int pageSize = 10);
     }
 }
--- src/BlogSphere.Data/Repositories/PostRepository.cs
+++ src/BlogSphere.Data/Repositories/PostRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BlogSphere.Core.Domain.Content;
+using BlogSphere.Core.Models;
 using BlogSphere.Core.Repositories;
 using BlogSphere.Data.SeedWorks;
 using Microsoft.EntityFrameworkCore;
@@ -11,9 +12,37 @@ namespace BlogSphere.Data.Repositories
         {
         }
 
-        public Task<List<Post>> GetPopularPostAsync(int count)
+        public Task<List<Post>> GetPopularPostsAsync(int count)
         {
             return _context.Posts.OrderByDescending(x => x.ViewCount).Take(count).ToListAsync();
+        }
+
+        public async Task<PagedResult<Post>> GetPostsPagingAsync(string keyword, Guid? categoryId, int pageIndex = 1, int pageSize = 10)
+        {
+            var query = _context.Posts.AsQueryable();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                query = query.Where(x => x.Name.Contains(keyword));
+            }
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(x => x.CategoryId == categoryId.Value);
+            }
+
+            var totalRow = await query.CountAsync();
+
+            query = query.OrderByDescending(x => x.DateCreated)
+                         .Skip((pageIndex - 1) * pageSize)
+                         .Take(pageSize);
+
+            return new PagedResult<Post>
+            {
+                Results = await query.ToListAsync(),
+                CurrentPage = pageIndex,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
         }
     }
 }
