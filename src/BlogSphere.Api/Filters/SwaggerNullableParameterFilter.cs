cfd4af790ea970cc705e9854676faa3c9ff8ee99
Minh Pham <giaminh2042004@gmail.com>
2025-11-16 15:12:57 +0700

    Refactor

--- src/BlogSphere.Api/Filters/SwaggerNullableParameterFilter.cs
+++ src/BlogSphere.Api/Filters/SwaggerNullableParameterFilter.cs
@@ -2,7 +2,7 @@
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-namespace BlogSphere.Api
+namespace BlogSphere.Api.Filters
 {
     public class SwaggerNullableParameterFilter : IParameterFilter
     {
